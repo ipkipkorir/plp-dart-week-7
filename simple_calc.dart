d6ea007d60b3cf9d57785a374bdbc1bb8b819061
montfortkorir <ipkipkorir@gmail.com>
2024-04-19 23:04:50 +0300

    Updated operations

--- simple_calc.dart
+++ simple_calc.dart
@@ -8,32 +8,52 @@ double inputNumber() {
 }
 
 // function to prompt the user to enter a math operand
-String inputMathOperand() {
-  print("\nPlease enter a math operand (+, _, *, /): \n");
-  String mathOperand = stdin.readLineSync()!;
-  return mathOperand;
+int inputMathOperand() {
+  print("\nPlease choose an operation:");
+  print("==============================");
+  print("1. Addition ( + )");
+  print("2. Subtraction ( - )");
+  print("3. Multiplication ( * )");
+  print("4. Division ( / )");
+  print("5. Modulo ( % )\n");
+  int choice = int.parse(stdin.readLineSync()!);
+  return choice;
 }
 
 void main() {
   // call the functions to implement the calculator
   double num1 = inputNumber();
   double num2 = inputNumber();
-  String operand = inputMathOperand();
+  int choice = inputMathOperand();
   double answer = 0;
+  String operand = "";
   // perform math operation per user's input
-  if (operand == "+") {
+  if (choice == 1) {
+    operand = "+";
+    print("Your choice: Addition");
     answer = num1 + num2;
-    print("${num1} ${operand} ${num2} = ${answer}");
-  } else if (operand == "-") {
+    print("\n${num1} ${operand} ${num2} = ${answer}\n");
+  } else if (choice == 2) {
+    operand = "-";
+    print("Your choice: Subtraction");
     answer = num1 - num2;
-    print("${num1} ${operand} ${num2} = ${answer}");
-  } else if (operand == "*") {
+    print("\n${num1} ${operand} ${num2} = ${answer}\n");
+  } else if (choice == 3) {
+    operand = "*";
+    print("Your choice: Multiplication");
     answer = num1 * num2;
-    print("${num1} ${operand} ${num2} = ${answer}");
-  } else if (operand == "/") {
+    print("\n${num1} ${operand} ${num2} = ${answer}\n");
+  } else if (choice == 4) {
+    operand = "/";
+    print("Your choice: Division");
     answer = num1 / num2;
-    print("${num1} ${operand} ${num2} = ${answer}");
+    print("\n${num1} ${operand} ${num2} = ${answer}\n");
+  } else if (choice == 5) {
+    operand = "%";
+    print("Your choice: Modulus");
+    answer = num1 % num2;
+    print("\n${num1} ${operand} ${num2} = ${answer}\n");
   } else {
-    print("Error occured. Enter valid inputs.");
+    print("\nError occured. Enter valid inputs.\n");
   }
 }
